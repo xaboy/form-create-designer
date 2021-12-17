43d0e52a9e6ee5b750331188906a306f7f94466d
YB-wang <136518847@qq.com>
2021-12-17 16:10:15 +0800

    栅格优化

--- src/config/rule/col.js
+++ src/config/rule/col.js
@@ -8,13 +8,14 @@ export default {
     rule() {
         return {
             type: name,
-            props: {span: 12},
+            props: {span: 12,xs:24},
             children: []
         };
     },
     props() {
         return [
             {type: 'slider', field: 'span', title: '栅格占据的列数', value: 12, props: {min: 0, max: 24}},
+            {type: 'slider', field: 'xs', title: '<768栅格占据的列数', value: 24, props: {min: 0, max: 24}},
             {type: 'slider', field: 'offset', title: '栅格左侧的间隔格数', props: {min: 0, max: 24}},
             {type: 'slider', field: 'push', title: '栅格向右移动格数', props: {min: 0, max: 24}},
             {type: 'slider', field: 'pull', title: '栅格向左移动格数', props: {min: 0, max: 24}}];
