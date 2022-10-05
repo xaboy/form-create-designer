2dc51c55f9cfd425f831c96529812ec954fdd3f3
xaboy <xaboy2005@qq.com>
2022-10-05 21:10:18 +0800

    更新依赖

--- examples/main.js
+++ examples/main.js
@@ -2,7 +2,7 @@ import {createApp} from 'vue';
 import ELEMENT from 'element-plus';
 import 'element-plus/dist/index.css';
 import formCreate from '@form-create/element-ui';
-import App from './App';
+import App from './App.vue';
 import FcDesigner from '../src/index';
 
 const app = createApp(App);
---
+++ vite.config.preview.js
@@ -0,0 +1,22 @@
+import {defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJSX from '@vitejs/plugin-vue-jsx'
+// https://vitejs.dev/config/
+export default defineConfig({
+    build: {
+        cssCodeSplit: true,
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    vue: ['vue'],
+                    'element-plus': ['element-plus'],
+                },
+                globals: {
+                    vue: 'Vue',
+                }
+            },
+        },
+        chunkSizeWarningLimit: 2000,
+    },
+    plugins: [vue(), vueJSX()]
+})
