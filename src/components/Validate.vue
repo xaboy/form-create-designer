c1386dae4208abfe19d45ace96b514a7910c6ed1
xaboy <xaboy2005@qq.com>
2024-09-13 15:33:57 +0800

    重新实现验证规则表单

--- src/components/Validate.vue
+++ src/components/Validate.vue
@@ -1,236 +1,160 @@
 <template>
-    <DragForm class="_fd-validate" :rule="rule" :option="option" :value="formValue"
-              @change="onInput"></DragForm>
+    <div class="_fd-validate">
+        <template v-for="(item, idx) in validate">
+            <div class="_fd-validate-item">
+                <div class="_fd-validate-title">
+                    <div>
+                        <span>{{ idx + 1 }}</span>
+                        {{ modes[item.mode] }}
+                    </div>
+                    <i class="fc-icon icon-delete2" @click="remove(idx)"></i>
+                </div>
+                <el-row>
+                    <el-col :span="getSpan(item)">
+                        <el-form-item :label="t('validate.mode')">
+                            <el-select v-model="item.trigger" @change="onInput">
+                                <el-option
+                                    v-for="item in triggers"
+                                    :key="item.value"
+                                    :label="item.label"
+                                    :value="item.value"
+                                />
+                            </el-select>
+                        </el-form-item>
+                    </el-col>
+                    <el-col :span="getSpan(item)">
+                        <el-form-item :label="modes[item.mode]">
+                            <template v-if="item.mode === 'pattern'">
+                                <elInput v-model="item[item.mode]" @change="onInput"></elInput>
+                            </template>
+                            <template v-else-if="item.mode === 'validator'">
+                                <FnInput v-model="item[item.mode]" name="name" :args="['rule', 'value', 'callback']"
+                                         @change="onInput">{{ t('validate.modes.validator') }}
+                                </FnInput>
+                            </template>
+                            <template v-else>
+                                <el-input-number v-model="item[item.mode]" @change="onInput"></el-input-number>
+                            </template>
+                        </el-form-item>
+                    </el-col>
+                    <el-col :span="24">
+                        <el-form-item :label="t('validate.message')">
+                            <el-input v-model="item.message" :placeholder="t('validate.requiredPlaceholder')"
+                                      @blur="onInput">
+                                <template #append>
+                                    <span class="append-msg"
+                                          @click="autoMessage(item)"> {{ t('validate.auto') }} </span>
+                                </template>
+                            </el-input>
+                        </el-form-item>
+                    </el-col>
+                </el-row>
+            </div>
+        </template>
+
+        <el-dropdown trigger="click" size="default" popper-class="_fd-validate-pop" @command="handleCommand">
+            <el-button class="_fd-validate-btn" size="small">{{ t('validate.rule') }} +</el-button>
+            <template #dropdown>
+                <el-dropdown-menu>
+                    <el-dropdown-item :command="value" v-for="(label, value) in modes" :key="value">
+                        <div>{{ label }}</div>
+                    </el-dropdown-item>
+                </el-dropdown-menu>
+            </template>
+        </el-dropdown>
+    </div>
 </template>
 
 <script>
-import {designerForm} from '../utils/form';
 import {defineComponent} from 'vue';
-import {deepCopy} from '@form-create/utils/lib/deepextend';
 import {localeOptions} from '../utils';
+import FnInput from './FnInput.vue';
+import {deepCopy} from '@form-create/utils/lib/deepextend';
 
 export default defineComponent({
     name: 'Validate',
     inject: ['designer'],
+    emits: ['input'],
     props: {
         value: Array,
     },
     components: {
-        DragForm: designerForm.$form(),
+        FnInput,
     },
     watch: {
         value(n) {
-            this.formValue = this.parseValue(n);
+            this.validate = this.parseValue(n || []);
         }
     },
     data() {
-        const t = this.designer.t;
-        const types = this.designer.activeRule._menu.validate || [];
-        const attrs = {
-            string: t('validate.types.string'),
-            array: t('validate.types.array'),
-            number: t('validate.types.number'),
-            integer: t('validate.types.integer'),
-            float: t('validate.types.float'),
-            object: t('validate.types.object'),
-            date: t('validate.types.date'),
-            url: t('validate.types.url'),
-            email: t('validate.types.email'),
-        };
-
-        const getOpts = lst => {
-            const opts = [];
-            lst && lst.forEach(k => {
-                opts.push({
-                    label: attrs[k],
-                    value: k
-                });
-            });
-            opts.push({
-                label: t('props.custom'),
-                value: 'validator'
-            });
-            return opts;
-        };
-        const opts = getOpts(types);
-
         return {
-            formValue: {},
-            t,
-            option: {
-                form: {
-                    labelPosition: 'top',
-                    size: 'mini',
-                    labelWidth: '90px'
-                },
-                submitBtn: false,
-                appendValue: true,
-                formData: this.parseValue(this.value)
-            },
-            rule: [
-                {
-                    type: 'group',
-                    field: 'validate',
-                    props: {
-                        expand: 1,
-                        sortBtn: false,
-                        defaultValue: {type: opts[0].value},
-                        rule: [
-                            {
-                                type: opts.length === 1 ? 'hidden' : 'select',
-                                field: 'type',
-                                value: '',
-                                title: t('validate.type'),
-                                props: {
-                                    placeholder: t('validate.typePlaceholder')
-                                },
-                                control: [
-                                    {
-                                        value: ['url', 'date', 'email', 'object', 'validator'],
-                                        condition: 'notIn',
-                                        rule: ['mode', 'min', 'max', 'len', 'pattern']
-                                    },
-                                    {
-                                        value: 'validator',
-                                        rule: ['validator']
-                                    },
-                                ],
-                                options: opts,
-                            },
-                            {
-                                type: 'select',
-                                title: t('validate.trigger'),
-                                field: 'trigger',
-                                value: 'change',
-                                options: localeOptions(t, [
-                                    {label: 'blur', value: 'blur'},
-                                    {label: 'change', value: 'change'},
-                                    {label: 'submit', value: 'submit'},
-                                ])
-                            },
-                            {
-                                type: 'FnEditor',
-                                field: 'validator',
-                                value: '',
-                                props: {
-                                    name: 'validator',
-                                    args: ['rule', 'value', 'callback'],
-                                    button: true,
-                                },
-                                style: 'height:300px;'
-                            },
-                            {
-                                type: 'select',
-                                title: t('validate.mode'),
-                                field: 'mode',
-                                options: [
-                                    {value: 'min', label: t('validate.modes.min')},
-                                    {value: 'max', label: t('validate.modes.max')},
-                                    {value: 'len', label: t('validate.modes.len')},
-                                    {value: 'pattern', label: t('validate.modes.pattern')},
-                                ],
-                                value: 'min',
-                                control: [
-                                    {
-                                        value: 'pattern',
-                                        rule: [
-                                            {
-                                                type: 'input',
-                                                field: 'pattern',
-                                                props: {
-                                                    size: 'mini'
-                                                },
-                                                title: t('validate.modes.pattern')
-                                            }
-                                        ]
-                                    },
-                                    {
-                                        value: 'min',
-                                        rule: [
-                                            {
-                                                type: 'inputNumber',
-                                                field: 'min',
-                                                title: t('validate.modes.min')
-                                            }
-                                        ]
-                                    },
-                                    {
-                                        value: 'max',
-                                        rule: [
-                                            {
-                                                type: 'inputNumber',
-                                                field: 'max',
-                                                title: t('validate.modes.max')
-                                            }
-                                        ]
-                                    },
-                                    {
-                                        value: 'len',
-                                        rule: [
-                                            {
-                                                type: 'inputNumber',
-                                                field: 'len',
-                                                title: t('validate.modes.len')
-                                            }
-                                        ]
-                                    },
-                                ]
-                            },
-                            {
-                                type: 'input',
-                                title: t('validate.message'),
-                                field: 'message',
-                                value: '',
-                                children: [
-                                    {
-                                        type: 'span',
-                                        slot: 'append',
-                                        inject: true,
-                                        class: 'append-msg',
-                                        on: {
-                                            click: (inject) => {
-                                                const title = this.designer.activeRule.title;
-                                                if (this.designer.activeRule) {
-                                                    inject.api.setValue('message', t(inject.api.form.mode !== 'required' ? 'validate.autoMode' : 'validate.autoRequired', {title}));
-                                                }
-                                            }
-                                        },
-                                        children: [t('validate.auto')]
-                                    }
-                                ]
-                            }
-                        ]
-                    },
-                    value: []
-                },
-            ]
+            validate: this.parseValue(this.value || []),
         };
     },
-    methods: {
-        onInput: function (field, value) {
-            const validate = deepCopy(value);
-            const modelValue = [];
-            (validate || []).forEach(v => {
-                if (!v || !Object.keys(v).length) {
-                    return false;
-                }
-                if (v.type === 'validator' && !v.validator) {
-                    v.validator = function(){}
-                }
-                const tmp = {...v};
-                if (!v.validator) {
-                    delete tmp.validator;
-                }
-                modelValue.push(tmp);
-            })
-            this.$emit('input', modelValue);
+    computed: {
+        t() {
+            return this.designer.t;
         },
-        parseValue(n) {
-            let val = {
-                validate: n ? [...n] : [],
-                type: n.length ? (n[0].type || 'string') : undefined
-            };
-            val.validate.forEach(v => {
+        modes() {
+            const activeRule = this.designer.activeRule;
+            if (activeRule && activeRule._menu.subForm === 'object') {
+                return {
+                    validator: this.t('validate.modes.validator'),
+                }
+            } else {
+                return {
+                    min: this.t('validate.modes.min'),
+                    max: this.t('validate.modes.max'),
+                    len: this.t('validate.modes.len'),
+                    pattern: this.t('validate.modes.pattern'),
+                    validator: this.t('validate.modes.validator'),
+                }
+            }
+        },
+        triggers() {
+            return localeOptions(this.t, [
+                {label: 'blur', value: 'blur'},
+                {label: 'change', value: 'change'},
+                {label: 'submit', value: 'submit'},
+            ]);
+        }
+    },
+    methods: {
+        handleCommand(mode) {
+            this.validate.push({
+                transform: new Function('val', 'this.type = val == null ? \'string\' : (Array.isArray(val) ? \'array\' : (typeof val)); return val;'),
+                mode,
+                trigger: 'blur'
+            });
+        },
+        autoMessage(item) {
+            const title = this.designer.activeRule.title;
+            if (this.designer.activeRule) {
+                item.message = this.t('validate.autoRequired', {title})
+                this.onInput();
+            }
+        },
+        getSpan(item) {
+            return ['pattern', 'validator', 'required'].indexOf(item.mode) > -1 ? 24 : 12;
+        },
+        onInput: function () {
+            this.$emit('input', this.validate.map(item => {
+                item = {...item};
+                if (!item.message) {
+                    delete item.message;
+                }
+                return item;
+            }));
+        },
+        remove(idx) {
+            this.validate.splice(idx, 1);
+            this.onInput();
+        },
+        parseValue(val) {
+            return deepCopy(val.map(v => {
+                if (v.validator) {
+                    v.mode = 'validator';
+                }
                 if (!v.mode) {
                     Object.keys(v).forEach(k => {
                         if (['message', 'type', 'trigger', 'mode'].indexOf(k) < 0) {
@@ -238,24 +162,85 @@ export default defineComponent({
                         }
                     });
                 }
-            });
-
-            return val;
+                return v;
+            }));
         }
     }
 });
 </script>
 
 <style>
+
+._fd-validate {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+}
+
+._fd-validate-btn {
+    font-weight: 400;
+    width: 100%;
+    border-color: #2E73FF;
+    color: #2E73FF;
+}
+
+._fd-validate-pop .el-dropdown-menu__item {
+    width: 248px;
+}
+
+._fd-validate-item {
+    border-bottom: 1px dashed #ECECEC;
+    margin-bottom: 10px;
+}
+
+._fd-validate-item .el-col-12:first-child {
+    padding-right: 5px;
+}
+
+._fd-validate-item .el-col-12 + .el-col-12 {
+    padding-left: 5px;
+}
+
+._fd-validate-item .el-input-number {
+    width: 100%;
+}
+
+._fd-validate-title {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin-bottom: 10px;
+}
+
+._fd-validate-title > div {
+    display: flex;
+    align-items: center;
+}
+
+._fd-validate-title > div > span {
+    width: 16px;
+    height: 16px;
+    background: #ECECEC;
+    text-align: center;
+    font-size: 12px;
+    line-height: 16px;
+    border-radius: 15px;
+    margin-right: 5px;
+}
+
+._fd-validate-title i {
+    cursor: pointer;
+}
+
+._fd-validate-title i:hover {
+    color: #FF2E2E;
+}
+
 ._fd-validate .append-msg {
     cursor: pointer;
 }
 
 ._fd-validate .el-input-group__append {
     padding: 0 10px;
-}
-
-._fd-validate .el-select {
-    width: 100%;
 }
 </style>
