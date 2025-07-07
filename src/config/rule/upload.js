08f63727e124f3b22f4eaeeae71d7935d82e6e1c
xaboy <xaboy2005@qq.com>
2025-07-07 11:06:51 +0800

    优化上传组件

--- src/config/rule/upload.js
+++ src/config/rule/upload.js
@@ -1,5 +1,5 @@
 import uniqueId from '@form-create/utils/lib/unique';
-import {getInjectArg, localeProps} from '../../utils';
+import {getInjectArg, localeOptions, localeProps} from '../../utils';
 
 const label = '上传';
 const name = 'upload';
@@ -22,8 +22,7 @@ export default {
             $required: false,
             props: {
                 action: '/',
-                uploadType: 'image',
-                onSuccess: new Function('res', 'file', 'file.url = res.data.url;')
+                onSuccess: '$FNX:const res = $inject.args[0];\nconst file = $inject.args[1];\n\nfile.url = res.data.url;'
             }
         };
     },
@@ -33,75 +32,98 @@ export default {
             field: 'disabled'
         }, {
             type: 'select',
-            field: 'uploadType',
-            options: [{label: t('props.image'), value: 'image'}, {
-                label: t('props.file'),
-                value: 'file'
-            }]
-        }, {type: 'switch', field: 'multiple'}, {
-            type: 'input',
-            field: 'action'
-        }, {
-            type: 'FnInput',
-            field: 'beforeUpload',
-            props: {
-                args: ['file'],
-                name: 'beforeUpload',
-            }
-        },  {
-            type: 'FnInput',
-            field: 'onRemove',
-            props: {
-                body: true,
-                button: true,
-                fnx: true,
-                args: [getInjectArg(t)],
-                name: 'onRemove',
-            }
-        }, {
-            type: 'FnInput',
-            field: 'onSuccess',
-            warning: t('com.upload.info'),
-            props: {
-                args: ['res', 'file'],
-                name: 'onSuccess',
-                button: true,
-            }
-        },  {
-            type: 'FnInput',
-            field: 'onPreview',
-            props: {
-                body: true,
-                button: true,
-                fnx: true,
-                args: [getInjectArg(t)],
-                name: 'onPreview',
-            }
-        }, {
-            type: 'TableOptions',
-            field: 'headers',
-            props: {
-                column: [{label: t('props.key'), key: 'label'}, {label: t('props.value'), key: 'value'}],
-                valueType: 'object'
-            }
-        }, {
-            type: 'TableOptions',
-            field: 'data',
-            props: {
-                column: [{label: t('props.key'), key: 'label'}, {label: t('props.value'), key: 'value'}],
-                valueType: 'object'
-            }
-        }, {type: 'input', field: 'name'}, {
-            type: 'switch',
-            field: 'withCredentials'
-        }, {type: 'input', field: 'accept'}, {
-            type: 'switch',
-            field: 'autoUpload',
-            value: true
-        }, {
-            type: 'inputNumber',
-            field: 'limit',
-            props: {min: 0},
-        }]);
+            field: 'listType',
+            options: localeOptions(t, [{label: 'text', value: 'text'}, {
+                label: 'picture',
+                value: 'picture'
+            }, {
+                label: 'picture-card',
+                value: 'picture-card'
+            }]),
+        }, {type: 'switch', field: 'multiple'}, {type: 'input', field: 'name'},
+            {
+                type: 'input',
+                field: 'accept',
+            },
+            {
+                type: 'input',
+                field: 'action'
+            }, {
+                type: 'FnInput',
+                field: 'beforeUpload',
+                props: {
+                    body: true,
+                    button: true,
+                    fnx: true,
+                    args: [getInjectArg(t)],
+                    name: 'beforeUpload',
+                }
+            }, {
+                type: 'FnInput',
+                field: 'beforeRemove',
+                props: {
+                    body: true,
+                    button: true,
+                    fnx: true,
+                    args: [getInjectArg(t)],
+                    name: 'beforeRemove',
+                }
+            }, {
+                type: 'FnInput',
+                field: 'onRemove',
+                props: {
+                    body: true,
+                    button: true,
+                    fnx: true,
+                    args: [getInjectArg(t)],
+                    name: 'onRemove',
+                }
+            }, {
+                type: 'FnInput',
+                field: 'onSuccess',
+                warning: t('com.upload.info'),
+                props: {
+                    body: true,
+                    button: true,
+                    fnx: true,
+                    args: [getInjectArg(t)],
+                    name: 'onSuccess',
+                }
+            }, {
+                type: 'FnInput',
+                field: 'onPreview',
+                props: {
+                    body: true,
+                    button: true,
+                    fnx: true,
+                    args: [getInjectArg(t)],
+                    name: 'onPreview',
+                }
+            }, {
+                type: 'TableOptions',
+                field: 'headers',
+                props: {
+                    column: [{label: t('props.key'), key: 'label'}, {label: t('props.value'), key: 'value'}],
+                    valueType: 'object'
+                }
+            }, {
+                type: 'TableOptions',
+                field: 'data',
+                props: {
+                    column: [{label: t('props.key'), key: 'label'}, {label: t('props.value'), key: 'value'}],
+                    valueType: 'object'
+                }
+            }, {
+                type: 'switch',
+                field: 'withCredentials'
+            }, {
+                type: 'switch',
+                field: 'autoUpload',
+                value: true
+            }, {
+                type: 'inputNumber',
+                field: 'limit',
+                props: {min: 0},
+            }]);
     }
 };
