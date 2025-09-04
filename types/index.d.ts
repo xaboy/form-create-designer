d3b5d939665a1c659578088d98ef7e73dab31cc2
xaboy <xaboy2005@qq.com>
2025-09-04 15:05:17 +0800

    粘贴组件时默认重置 field 和 name(不向前兼容的更新)

--- types/index.d.ts
+++ types/index.d.ts
@@ -72,6 +72,8 @@ export interface Config {
     autoActive?: boolean;
     //是否在复制时自动重置组件的name,默认开启
     autoResetName?: boolean;
+    //是否在复制时自动重置组件的field,默认开启
+    autoResetField?: boolean;
     //删除组件前置事件, 返回 false 终止删除
     beforeRemoveRule?: (data: { rule: Rule }) => false|void;
     //选中组件前置事件, 返回 false 终止选中
