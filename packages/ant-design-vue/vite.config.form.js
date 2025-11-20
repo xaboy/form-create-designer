15467d5a0ca321f83146e750589c3e3e4ce6f9e9
xaboy <xaboy2005@qq.com>
2025-11-20 17:47:06 +0800

    组件导出单独的组件包, 方便跨项目使用

---
+++ packages/ant-design-vue/vite.config.form.js
@@ -0,0 +1,86 @@
+import {defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJSX from '@vitejs/plugin-vue-jsx'
+import banner from 'vite-plugin-banner'
+import cssnano from 'cssnano'
+import visualizer from 'rollup-plugin-visualizer';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+import {author, license, name, version} from './package.json'
+
+const extnedsPlugins = [];
+
+function getBanner(banner, pkg) {
+    if (!banner || typeof banner === 'string') {
+        return banner || '';
+    }
+
+    banner = {...pkg, ...(banner === true ? {} : banner)};
+
+    const author =banner.author
+
+    const license = banner.license || '';
+    return (
+        '/*!\n' +
+        ' * FormCreate 可视化表单设计器\n' +
+        ` * ${banner.name} v${banner.version}\n` +
+        ` * (c) ${author || ''}\n` +
+        (license && ` * Released under the ${license} License.\n`) +
+        ' */'
+    );
+}
+
+const __banner__ = {
+    author: `2021-${new Date().getFullYear()} ${author}\n * Github https://github.com/xaboy/form-create-designer\n * Site https://form-create.com/`,
+    license,
+    name,
+    version
+}
+
+// 打包生产环境才引入的插件
+// if (process.env.NODE_ENV === 'production') {
+//     // 打包依赖展示
+//     extnedsPlugins.push(
+//         visualizer({
+//             open: true,
+//             gzipSize: true,
+//             brotliSize: true,
+//         })
+//     );
+// }
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    build: {
+        emptyOutDir: false,
+        lib: {
+            entry: 'src/form/index.js',
+            name: 'FcDesignerComponents',
+            fileName: format => `components.${format}.js`,
+        },
+        rollupOptions: {
+            output: {
+                exports: 'named',
+                globals: {
+                    vue: 'Vue',
+                    '@form-create/ant-design-vue': 'formCreate'
+                }
+            },
+            external: [
+                'vue',
+                '@form-create/ant-design-vue'
+            ],
+
+        },
+        brotliSize: true
+    },
+    css: {
+        postcss: {
+            plugins: [
+                cssnano({
+                    preset: 'advanced'
+                })
+            ]
+        }
+    },
+    plugins: [vue(),  vueJSX(), banner(getBanner(__banner__)),cssInjectedByJsPlugin(), ...extnedsPlugins]
+})
---
+++ packages/vant/src/form/index.js
@@ -0,0 +1,18 @@
+import fromCreateMobile from '@form-create/vant';
+import FcEditor from '@form-create/component-wangeditor';
+import FcTitle from '../components/FcTitle.vue';
+import SignaturePad from '../components/SignaturePad.vue';
+import TableForm from '../components/tableForm/TableForm.vue';
+import Table from '../components/table/Table.vue';
+
+const install = (formCreate) => {
+    formCreate.component('FcEditor', FcEditor);
+    formCreate.component('FcTitle', FcTitle);
+    formCreate.component('SignaturePad', SignaturePad);
+    formCreate.component('TableForm', TableForm);
+    formCreate.component('FcTable', Table);
+}
+
+install(fromCreateMobile);
+
+export default install;
